c302d0831635da082101696a0263e83199669548
adicohen26 <106975136+adicohen26@users.noreply.github.com>
2022-09-07 23:53:20 +0300

    Update Service.jsx

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -8,7 +8,7 @@ import Header from "../Header";
 
 function Service(props){
     const { service } = useParams();
-    const url=process.env.REACT_APP_BASE_URL+"/services/"+service;
+    const url="https://sharonandlironbe.azurewebsites.net"+"/services/"+service;
     const {data,isPending,error}=useFetch(url);
     const [isOpen,setIsOpen]=useState(false);
     const [selectedCard, setSelectedCard]=useState("");
